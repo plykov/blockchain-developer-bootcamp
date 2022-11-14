52a69ba466a18c4e67e5657b707a431f37eba476
plykov <plykov@gmail.com>
2022-11-14 13:32:54 +0300

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,21 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "Pavel's Token";
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    
+    //Track balances
+    mapping(address => uint256) public balanceOf;
+    //Send Tokens
+
+
+    
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,28 +1,43 @@
-const { expect } = require('chai')
-const { ethers } = require('hardhat')
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
 describe('Token', ()=> {
-    let token
+    let token, accounts, deployer
 
     beforeEach(async() => {
         const Token = await ethers.getContractFactory('Token')
-        token = await Token.deploy()
+        token = await Token.deploy('My Token', 'DAPP', '1000000')
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
+
+describe('Deployment', () => {
+    const name = 'My Token'
+    const symbol = 'DAPP'
+    const decimals = '18'
+    const totalSupply = tokens('1000000')
 
     it('has correct name', async ()=> {
-        //Fetch token from blockchain
-
-        //Read token name
-        const name = await token.name() 
-        //Check that name is correct
-        expect(name).to.equal("Pavel's Token")
+        expect(await token.name()).to.equal(name)
     })
     it('has correct symbol', async ()=> {
-        //Fetch token from blockchain
-
-        //Read token symbol
-        const symbol = await token.symbol() 
-        //Check that symbol is correct
-        expect(symbol).to.equal("PLT")
+        expect(await token.symbol()).to.equal(symbol)
     })
+    it('has correct decimals', async ()=> {
+        expect(await token.decimals()).to.equal(decimals)
+    })
+    it('has correct total supply', async ()=> {
+        expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+    it('assigns total supply to deployer', async ()=> {
+        console.log()
+        expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+    })
+}) 
+
 })
