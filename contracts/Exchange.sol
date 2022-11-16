1024926cc0dbafea813e39300062e1ebec985caa
plykov <plykov@gmail.com>
2022-11-16 19:06:19 +0300

    8. Create Exchange

---
+++ contracts/Exchange.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Exchange
+{
+    address public feeAccount;
+    uint256 public feePercent;
+
+
+    constructor(address _feeAccount, uint256 _feePercent) 
+    {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+
+    }
+}
