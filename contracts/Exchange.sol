7a684ce9454a66e94af0403d56e2ee560bee788e
plykov <plykov@gmail.com>
2022-11-18 13:50:11 +0300

    11. Make orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -9,14 +9,42 @@ contract Exchange
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;//0
 
-    event Deposit(address token, address user, uint256 amount, uint256 balance);
+
+    event Deposit(
+        address token, 
+        address user, 
+        uint256 amount, 
+        uint256 balance
+    );
     event Withdraw(
         address token,
         address user,
         uint256 amount,
         uint256 balance
     );
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    struct _Order {
+        //Attributes of the order
+        uint256 id; //unique identifier
+        address user; //user who made order
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp; //when order was created
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) 
     {
@@ -58,5 +86,41 @@ contract Exchange
     {
         return tokens[_token][_user];
     }
+//--------------------
+//MAKE & CANCEL ORDERS
+    function makeOrder(
+        address _tokenGet, 
+        uint256 _amountGet, 
+        address _tokenGive, 
+        uint256 _amountGive) 
+        public 
+        {
+        //require token balance
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);        
+        //create ordeer
+        orderCount = orderCount + 1;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+            );
+        //Emit the event
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+            );
+
+        }
+
+
 
 }
